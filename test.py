59a217ccb01e4521bc3acd0ce5a63ac2034990f8
Chris Anderson <chris.anderson@byu.net>
2016-03-19 15:22:24 -0600

    initial commit

---
+++ test.py
@@ -0,0 +1,19 @@
+from mido import MidiFile
+from mido.midifiles import MidiTrack
+
+with MidiFile() as new_mid:
+    new_track = MidiTrack()
+
+    mid = MidiFile('tnfdm.mid')
+
+    print('mid', mid)
+    for i, track in enumerate(mid.tracks):
+        print('len(track)', len(track))
+        for message in track:
+            print('message', message)
+            new_track.append(message)
+
+    new_mid.tracks.append(new_track)
+
+    print('ALL TRACKS APPENDED')
+    new_mid.save('new_song.mid')
