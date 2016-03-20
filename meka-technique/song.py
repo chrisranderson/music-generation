572ccbad27d6d4d080cd910b92e5f1990c1c2f18
Daniel Saunders <dsaunders410@gmail.com>
2016-03-20 15:53:24 -0600

    Add mido to song.py.

--- meka-technique/song.py
+++ meka-technique/song.py
@@ -1,4 +1,18 @@
+from mido import MidiFile
+from mido.midifiles import MidiTrack
+
 class Song:
 
     def __init__(self):
         self.notes = []
+
+    def read_file(self, file):
+        new_track = MidiTrack()
+
+        mid = MidiFile(file)
+
+        for i, track in enumerate(mid.tracks):
+            for message in track:
+                new_track.append(message)
+
+        new_mid.tracks.append(new_track)
