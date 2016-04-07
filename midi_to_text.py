fdf8456c8d48ab0d2ec7a022d854e43769115bb1
Russell <2russellsmith@gmail.com>
2016-04-07 10:09:15 -0600

    Condensed file length in training files

--- midi_to_text.py
+++ midi_to_text.py
@@ -26,7 +26,24 @@ for fileName in os.listdir('midi'):
         for i, track in enumerate(mid.tracks):
             for message in track:
                 if good_message(str(message)):
-                    total_text += (str(message) + '\n')
+                    smessage = str(message).split()
+                    #c~0 n!1 v@2 t#3
+                    if(smessage[0] == "control_change"):
+                        total_text += ("cc c~" + smessage[1].split('=')[1] + " co^" + smessage[2].split('=')[1] + " va@"
+                        + smessage[3].split('=')[1] + " t#" + smessage[3].split('=')[1])
+                    elif(smessage[0] == "note_on"):
+                        total_text += ("no c~" + smessage[1].split('=')[1] + " n!" + smessage[2].split('=')[1] + " ve%"
+                        + smessage[3].split('=')[1] + " t#" + smessage[3].split('=')[1])
+
+                    elif(smessage[0] == "note_off"):
+                        total_text += ("nf c~" + smessage[1].split('=')[1] + " n!" + smessage[2].split('=')[1] + " ve%"
+                        + smessage[3].split('=')[1] + " t#" + smessage[3].split('=')[1])
+
+                    elif(smessage[0] == "pitchwheel"):
+                        total_text += ("pw c~" + smessage[1].split('=')[1] + " p*" + smessage[2].split('=')[1] \
+                                      + " t#" + smessage[3].split('=')[1])
+
+                    total_text += '\n'
 
     output_file.write(total_text)
     output_file.close()
