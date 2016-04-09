47e1e4a5626f787a3d5926dca6ee81f7d59edd97
chrisranderson <chris.anderson@byu.net>
2016-04-09 09:33:35 -0600

    -

--- text_to_midi.py
+++ text_to_midi.py
@@ -1,43 +1,61 @@
 from mido import MidiFile
 from mido.midifiles import MidiTrack
-from mido import Message
+from mido import Message, MetaMessage
 import time
 import datetime
 
 with MidiFile() as new_mid:
     new_track = MidiTrack()
 
-    with open("LearningText/appass_2_format0.mid.txt") as f:
+    filename = "generated-music/round2-2.txt"
+    with open(filename) as f:
+
+        new_track.append(MetaMessage('set_tempo', tempo=500000*3))
         for line in f:
 
             parts = line.split()
             #c~0 n!1 v@2 t#3
             if parts[0] == "pw":
                 if abs(int(float(parts[2].split('=')[1]))) < 8191:
-                    new_track.append(Message('pitchwheel', channel=int(float(parts[1].split('~')[1])),
-                                         pitch=int(float(parts[2].split('*')[1])),
-                                         time=int(float(parts[3].split('#')[1]))))
+                    new_track.append(Message('pitchwheel', channel=0,
+                                         pitch=int(float(parts[1].split('*')[1])),
+                                         time=int(float(parts[2].split('#')[1]))))
             elif parts[0] == "no":
-                new_track.append(Message('note_on', channel=int(float(parts[1].split('~')[1])),
-                                         note=int(float(parts[2].split('!')[1])),
-                                         velocity=int(float(parts[3].split('%')[1])),
-                                         time=int(float(parts[4].split('#')[1]))))
+                velocity = int(float(parts[2].split('%')[1]))
+                velocity = velocity if velocity <= 127 else 127
+                t = int(float(parts[3].split('#')[1]))
+                t = t if t <= 127 else 127
+
+                new_track.append(Message('note_on', channel=0,
+                                         note=int(float(parts[1].split('!')[1])),
+                                         velocity=velocity,
+                                         time=t))
             elif parts[0] == "nf":
-                new_track.append(Message('note_on', channel=int(float(parts[1].split('~')[1])),
-                                         note=int(float(parts[2].split('!')[1])),
-                                         velocity=int(float(parts[3].split('%')[1])),
-                                         time=int(float(parts[4].split('#')[1]))))
-            # elif parts[0] == "cc":
-            #     new_track.append(Message('control_change', channel=int(float(parts[1].split('~')[1])),
-            #                              control=int(float(parts[2].split('^')[1])),
-            #                              value=int(float(parts[3].split('@')[1])),
-            #                              time=int(float(parts[4].split('#')[1]))))
+                velocity = int(float(parts[2].split('%')[1]))
+                velocity = velocity if velocity <= 127 else 127
+                t = int(float(parts[3].split('#')[1]))
+                t = t if t <= 127 else 127
+
+                new_track.append(Message('note_off', channel=0,
+                                         note=int(float(parts[1].split('!')[1])),
+                                         velocity=velocity,
+                                         time=t))
+            elif parts[0] == "cc":
+                value = int(float(parts[2].split('@')[1]))
+                value = value if value <= 127 else 127
+                t = int(float(parts[3].split('#')[1]))
+                t = t if t <= 127 else 127
+
+                new_track.append(Message('control_change', channel=0,
+                                         control=int(float(parts[1].split('^')[1])),
+                                         value=value,
+                                         time=t))
             else:
                 print(parts[0])
 
     new_mid.tracks.append(new_track)
 
     print('ALL TRACKS APPENDED')
-    new_mid.save(str(datetime.date.today()) + ':' + str(time.time())[-8:-3] + '.mid')
+    new_mid.save(filename[:-4] + '.mid')
 
 
